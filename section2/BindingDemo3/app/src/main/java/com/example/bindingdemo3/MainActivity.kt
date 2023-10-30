e481b59fb1544e748fc25fa7bbbc224ff2215ea0
Tnalxmsk <alstnwkd990@naver.com>
2023-10-30 22:07:38 +0900

    Section 2.DataBinding -8.객체와의 데이터 바인딩 적용!

--- section2/BindingDemo3/app/src/main/java/com/example/bindingdemo3/MainActivity.kt
+++ section2/BindingDemo3/app/src/main/java/com/example/bindingdemo3/MainActivity.kt
@@ -10,9 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-        val student =getStudent()
+        /*val student =getStudent()
         binding.nameText.text = student.name
-        binding.emailText.text = student.email
+        binding.emailText.text = student.email*/
+        binding.student = getStudent()
     }
 
     private fun getStudent():Student{

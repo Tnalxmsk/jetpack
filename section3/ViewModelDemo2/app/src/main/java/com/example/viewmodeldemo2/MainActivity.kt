acb176732414147a368871e7a9bdb1a3d2d8e4f7
Tnalxmsk <alstnwkd990@naver.com>
2023-11-01 23:20:36 +0900

    Section3.ViewModel
    ViewModel Factory Final

--- section3/ViewModelDemo2/app/src/main/java/com/example/viewmodeldemo2/MainActivity.kt
+++ section3/ViewModelDemo2/app/src/main/java/com/example/viewmodeldemo2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.viewmodeldemo2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ViewSwitcher.ViewFactory
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.viewmodeldemo2.databinding.ActivityMainBinding
@@ -9,10 +10,12 @@ import com.example.viewmodeldemo2.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainActivityViewModel
+    private lateinit var viewFactory: MainActivityVewModelFactory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        viewFactory = MainActivityVewModelFactory(125)
         binding.resultTextView.text = viewModel.getTotal().toString()
 
         binding.insertButton.setOnClickListener {
